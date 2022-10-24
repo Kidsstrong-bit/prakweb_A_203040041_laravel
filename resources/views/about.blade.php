d2f9b471694f517764fb15dd5139a83fd0bbb04a
AlfiS.R <alfisyahrin673@gmail.com>
2022-10-24 14:48:53 +0700

    Laravel video 4

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.main')
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
-  <title>ALS BLOG | About</title>
-</head>
-
-<body>
-  <h1>Halaman About</h1>
-  <h3><?php echo $name; ?></h3>
-  <p><?php echo $email; ?></p>
-  <img src="img/<?= $image; ?>" alt="Alfi Syahrin" width="200">
-</body>
-
-</html>
+@section('container')
+    <h1>Halaman About</h1>
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="250" class="img-thumbnail rounded-circle">
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,69 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Alfi Syahrin R",
         "email" => "alfisyah672@gmail.com",
         "image" => "als.jpg"
     ]);
 });
 
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Alfi Syahrin R",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dignissimos deserunt atque voluptatibus expedita cumque molestias! Eligendi et iusto minima sed sapiente quo tenetur vel doloribus repellat? Unde animi iste debitis consequuntur sint dignissimos incidunt tempore earum aspernatur repellat temporibus saepe neque aperiam soluta veritatis, nemo hic quos quam quis delectus aliquid libero velit? Delectus voluptatum cumque cum numquam consequuntur odit ducimus obcaecati ab! Excepturi veritatis, id culpa tempora voluptatem amet beatae illo vitae dicta eaque sequi corporis, itaque aut!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Mamank Ginanjar",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus, nisi odio. Corrupti architecto magnam dignissimos, et optio blanditiis, rerum non id quidem quo vel, aliquid voluptatum maiores. Blanditiis accusamus odit, laudantium voluptatibus officiis a ducimus perspiciatis neque dolorem, exercitationem molestiae nemo beatae voluptate, numquam odio quia suscipit at. Nobis, itaque nemo. Enim, autem unde quidem, tempora eligendi nesciunt odio blanditiis aliquid necessitatibus recusandae a, eum repudiandae! Minima voluptas, repellat nulla suscipit corrupti blanditiis! Temporibus optio possimus officiis! Reprehenderit sunt fugit voluptate minus quos, adipisci, fugiat aut voluptatem eligendi laudantium maxime iure quae harum nobis! Voluptatem ex minus non fuga repellendus!"
+        ],
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Alfi Syahrin R",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dignissimos deserunt atque voluptatibus expedita cumque molestias! Eligendi et iusto minima sed sapiente quo tenetur vel doloribus repellat? Unde animi iste debitis consequuntur sint dignissimos incidunt tempore earum aspernatur repellat temporibus saepe neque aperiam soluta veritatis, nemo hic quos quam quis delectus aliquid libero velit? Delectus voluptatum cumque cum numquam consequuntur odit ducimus obcaecati ab! Excepturi veritatis, id culpa tempora voluptatem amet beatae illo vitae dicta eaque sequi corporis, itaque aut!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Mamank Ginanjar",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus, nisi odio. Corrupti architecto magnam dignissimos, et optio blanditiis, rerum non id quidem quo vel, aliquid voluptatum maiores. Blanditiis accusamus odit, laudantium voluptatibus officiis a ducimus perspiciatis neque dolorem, exercitationem molestiae nemo beatae voluptate, numquam odio quia suscipit at. Nobis, itaque nemo. Enim, autem unde quidem, tempora eligendi nesciunt odio blanditiis aliquid necessitatibus recusandae a, eum repudiandae! Minima voluptas, repellat nulla suscipit corrupti blanditiis! Temporibus optio possimus officiis! Reprehenderit sunt fugit voluptate minus quos, adipisci, fugiat aut voluptatem eligendi laudantium maxime iure quae harum nobis! Voluptatem ex minus non fuga repellendus!"
+        ],
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single post",
+        "post" => $new_post
+    ]);
 });
